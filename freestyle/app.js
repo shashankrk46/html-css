bf64ed6b9d7724757adc28dcba553dd73c27925d
shashankrk46 <shashankrk46@gmail.com>
2020-10-14 22:41:42 +0530

    added animation

--- freestyle/app.js
+++ freestyle/app.js
@@ -1,1 +1,11 @@
+function createID(firstname, lastname) {
+  let a = firstname[0].toLowerCase();
 
+  let b = lastname[0].toUpperCase();
+
+  let c = lastname.slice(1, 3).toLowerCase();
+
+  return `${a}${b}${c}`
+}
+
+console.log(createID("Jary", "lamb"))
